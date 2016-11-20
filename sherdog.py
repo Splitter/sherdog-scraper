392f867bf7fb7e16982fc76212092bd4cc873f2e
Splitter <splitvisionz@gmail.com>
2016-11-19 20:03:20 -0500

    Indentation error fix again, hopefully it persists from my machine to github

--- sherdog.py
+++ sherdog.py
@@ -237,22 +237,21 @@ class Event(LazySherdogObject):
             return Fight(event=self, fighters=fighters,
                     winner=None, match=None, method=None, referee=None,
                     round=None, time=None)
-
-		try:
-			# parse match, method, ref, round, time
-			td = dom.find('table', {'class':'resume'}).findAll('td')
-			keys = [x.contents[0].text.lower() for x in td]
-			values = [x.contents[-1].lstrip() for x in td]
-			info = dict(zip(keys, values))
-			time = self._parse_fight_time(info['time'])
-			return Fight(event=self, fighters=fighters,
-					winner=self._fight_winner(result, left_fighter, right_fighter),
-					match=int(info['match']), method=info['method'], referee=info['referee'],
-					round=int(info['round']), time=time)
-		except:
-					return Fight(event=self, fighters=fighters,
-							winner=None, match=None, method=None, referee=None,
-							round=None, time=None)
+        try:
+                # parse match, method, ref, round, time
+                td = dom.find('table', {'class':'resume'}).findAll('td')
+                keys = [x.contents[0].text.lower() for x in td]
+                values = [x.contents[-1].lstrip() for x in td]
+                info = dict(zip(keys, values))
+                time = self._parse_fight_time(info['time'])
+                return Fight(event=self, fighters=fighters,
+                        winner=self._fight_winner(result, left_fighter, right_fighter),
+                        match=int(info['match']), method=info['method'], referee=info['referee'],
+                        round=int(info['round']), time=time)
+        except:
+                return Fight(event=self, fighters=fighters,
+                                winner=None, match=None, method=None, referee=None,
+                                round=None, time=None)
 			
 
     def _parse_sub_fight(self, row):
